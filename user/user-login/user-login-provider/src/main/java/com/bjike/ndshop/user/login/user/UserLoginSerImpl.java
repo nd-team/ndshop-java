b61291636247da0004cb1eed9559544dccf8f481
huanghuanlai <102535481@qq.com>
2016-11-01 14:52:38 +0800

    删除导入不存在的包

--- user/user-login/user-login-provider/src/main/java/com/bjike/ndshop/user/login/user/UserLoginSerImpl.java
+++ user/user-login/user-login-provider/src/main/java/com/bjike/ndshop/user/login/user/UserLoginSerImpl.java
@@ -2,7 +2,7 @@ package com.bjike.ndshop.user.login.user;
 
 
 import com.bjike.ndshop.user.common.api.entity.User;
-import com.bjike.ndshop.user.login.api.service.IUserLoginSer;
+import com.bjike.ndshop.user.login.service.IUserLoginSer;
 import com.dounine.corgi.rpc.spring.annotation.Service;
 
 /**
@@ -10,6 +10,7 @@ import com.dounine.corgi.rpc.spring.annotation.Service;
  */
 @Service(version = "1.0.0")
 public class UserLoginSerImpl implements IUserLoginSer{
+
     @Override
     public void login(User user) {
 
