ce60fe34a4d326dba52a5b73e53bbee113f62b9d
liguiqin <xinaml@qq.com>
2016-11-01 14:44:13 +0800

    修改目录结构

---
+++ user/user-login/user-login-provider/src/main/java/com/bjike/ndshop/user/login/user/UserLoginSerImpl.java
@@ -0,0 +1,17 @@
+package com.bjike.ndshop.user.login.user;
+
+
+import com.bjike.ndshop.user.common.api.entity.User;
+import com.bjike.ndshop.user.login.api.service.IUserLoginSer;
+import com.dounine.corgi.rpc.spring.annotation.Service;
+
+/**
+ * Created by huanghuanlai on 16/5/24.
+ */
+@Service(version = "1.0.0")
+public class UserLoginSerImpl implements IUserLoginSer{
+    @Override
+    public void login(User user) {
+
+    }
+}
---
+++ user/user-register/user-register-provider/src/main/java/com/bjike/ndshop/user/register/boot/App.java
@@ -0,0 +1,17 @@
+package com.bjike.ndshop.user.register.boot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * Created by huanghuanlai on 16/8/16.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.bjike.ndshop.user.register.provider",
+        excludeFilters = {@ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                value = {Configuration.class})})
+public class App {
+
+}
