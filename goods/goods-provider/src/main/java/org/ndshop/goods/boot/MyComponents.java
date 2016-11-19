a58e011a173a471072003cf40e56a7f3076d97d0
ike <ike@localhost.localdomain>
2016-11-19 09:05:12 +0800

    自定义商品的缓存名

--- goods/goods-provider/src/main/java/org/ndshop/goods/boot/MyComponents.java
+++ goods/goods-provider/src/main/java/org/ndshop/goods/boot/MyComponents.java
@@ -1,42 +1,17 @@
 package org.ndshop.goods.boot;
 
 import org.ndshop.dbs.jpa.boot.initializer.Components;
-import org.ndshop.user.common.entity.User;
-import org.springframework.cache.Cache;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.ndshop.dbs.jpa.boot.initializer.EntityToScan;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * Created by lgq on 16-10-13.
- */
+
 @Component
-public class MyComponents extends Components {
-
-
+public class MyComponents extends Components implements EntityToScan {
     @Override
-    public String[] scanPackages() {
+    public String[] entityScan() {
         return new String[]{"org.ndshop.goods","org.ndshop.user"};
     }
-
-    public static void main(String[] args) {
-        System.out.println( User.class.getPackage());
-    }
-    @Override
-    public List<Cache> initCaches() {
-        List<Cache> list = new ArrayList<>();
-        ConcurrentMapCache cache =  new ConcurrentMapCache("goodsServiceCache");
-        ConcurrentMapCache daoCache =  new ConcurrentMapCache("goodsDaoCache");
-        list.add( cache );
-        list.add( daoCache );
-//        return Arrays.asList(cache);
-        return list;
-    }
-//    public static String clazzForPackage(Class<?> clazz,int parent){
-//
-//
-//    }
-
 }
+
+
